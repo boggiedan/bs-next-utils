a2782ac66011121c5f6944a113145b06ebe1122d
bogdan.sukmanov <bogdan.sukmanov@doclerholding.com>
2023-04-06 16:09:01 +0200

    chore: cleanup

--- src/components/AlertsProvider.tsx
+++ src/components/AlertsProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, FC, ReactNode, useCallback, useEffect, useState } from "react";
+import { createContext, FC, ReactNode, useEffect, useState } from "react";
 import cx from "classnames";
 import { Nullable } from "@/utils/types";
 
