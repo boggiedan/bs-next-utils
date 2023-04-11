46744bb6569d92d3e8fc73b7c0637c44bc94fe02
bogdan.sukmanov <bogdan.sukmanov@doclerholding.com>
2023-04-11 17:07:38 +0200

    feat: make util for hidable classes

--- components/DataTable/utils.ts
+++ components/DataTable/utils.ts
@@ -1,10 +1,45 @@
 import { HidableSizesProps } from "@/components/DataTable/types";
 
-export const getHideableClasses = (sizes: HidableSizesProps) =>
-  Object.keys(sizes)
+const hideFromLg: HidableSizesProps = {
+  lgHidden: true,
+  mdHidden: true,
+  smHidden: true,
+  xsHidden: true,
+};
+
+const hideFromMd: HidableSizesProps = {
+  mdHidden: true,
+  smHidden: true,
+  xsHidden: true,
+};
+
+const hideFromSm: HidableSizesProps = {
+  smHidden: true,
+  xsHidden: true,
+};
+
+const makeHideClass = (sizes: HidableSizesProps) => {
+  return Object.keys(sizes)
     .map((key) => {
       const size = key.slice(0, 2);
 
       return `max-${size}:hidden`;
     })
     .join(" ");
+};
+
+export const getHideableClasses = (sizes: HidableSizesProps) => {
+  if (sizes.lgHidden) {
+    return makeHideClass(hideFromLg);
+  }
+
+  if (sizes.mdHidden) {
+    return makeHideClass(hideFromMd);
+  }
+
+  if (sizes.smHidden) {
+    return makeHideClass(hideFromSm);
+  }
+
+  return makeHideClass(sizes);
+};
